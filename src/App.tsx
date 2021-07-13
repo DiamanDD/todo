020e84f7c2b4f816b1a91ae01396a109bfb179a7
DiamanDD <66356974+DiamanDD@users.noreply.github.com>
2021-07-13 08:50:32 +0300

    homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {taskType, TodoList} from "./TodoList";
+import {AccordionTitle} from "./AccordionTitle";
+import {AccordionBody} from "./AccordionBody";
 
-function App() {
-  return (
-    <div className="App">ПРо,ный коммит
+
+function App(){
+
+let tasks1:Array<taskType>  = [
+    {id: 1, title: "CSS", isDone: true },
+    {id: 2, title: "JS", isDone: true },
+    {id: 3, title: "React", isDone: false }
+];
+    let tasks2:Array<taskType>  = [
+        {id: 1, title: "CSS1", isDone: false },
+        {id: 2, title: "JS2", isDone: true },
+        {id: 3, title: "React3", isDone: false }
+    ];
+return (
+    <div className="App">
+        <TodoList titles={"What to learn 1"} title={tasks1}/>
+        <TodoList  titles={"What to learn 2"} title={tasks2}/>
+        <AccordionTitle/>
+        <AccordionBody/>
 
 
-      dfsdf
+
     </div>
-  );
+);
 }
 
 export default App;
